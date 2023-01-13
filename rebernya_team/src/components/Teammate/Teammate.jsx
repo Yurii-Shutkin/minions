af3439de938ec082eb14916180276661e679dc02
Yurii-Shutkin <yurashutkinn@gmail.com>
2023-01-13 13:45:53 +0200

    added animations

--- rebernya_team/src/components/Teammate/Teammate.jsx
+++ rebernya_team/src/components/Teammate/Teammate.jsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './Teammate.css'
 
 export default function Teammate({name, color, img, sound}) {
+  const [imgClass, setImgClass] = useState('');
+  const [itemClass, setItemClass] = useState('');
+
+  const animFunc = (cl, state) => {
+    state(cl);
+    setTimeout(() => {
+      state('')
+    }, 1000)
+  }
+
   const play = au => {
     const aud = new Audio(au)
     aud.play()
+    console.log(aud)
+  }
+
+  const complexFunc = () => {
+    animFunc('teammate-anim', setItemClass);
+    animFunc('team-img-anim', setImgClass);
+    play(sound);
   }
 
   return (
-    <div className='teammate' style={{background: color}} onClick={() => play(sound)}>
+    <button className={`teammate ${itemClass}`} style={{background: color}} onClick={complexFunc}>
       <div className='team-item'>
-        <img className='team-img' src={img} alt="" />
+        <img className={`team-img ${imgClass}`} src={img} alt="" />
         <span className='team-name'>{name}</span>
       </div>
-      <audio src={sound} type='audio/ogg'></audio>
-    </div>
+      <audio src={sound} type='audio/mp3'></audio>
+    </button>
   )
 }
